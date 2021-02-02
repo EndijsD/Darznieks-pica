2b9aad92df89155a61eb8a3e1552b267dd78a960
Endijs Dārznieks <endijsdarznieks@gmail.com>
2021-02-02 13:43:05 +0200

    Pievienota izvēlne

--- Pica/src/pica.java
+++ Pica/src/pica.java
@@ -1,7 +1,44 @@
+import javax.swing.JOptionPane;
 
 public class pica {
 
+	
+	static void informācija() {
+		
+	}
+	static void lielums() {
+		
+	}
+	static void tips() {
+		
+	}
+	static void virskārta() {
+		
+	}
+	static void summa() {
+		
+	}
 	public static void main(String[] args) {
-		System.out.print("Sākotnējā versija");
+		String izvele;
+		
+		do{
+			izvele = JOptionPane.showInputDialog("1 - Pasūtītāja informācija | 2 - Picas lielums | 3 - Picas tips | 4 - Picas virskārtas/piedevas | 5 - Gala summa | x - Apturēt programmu");
+			
+			switch(izvele.toLowerCase()){
+				case "1": informācija(); break;
+				
+				case "2": lielums(); break;
+				
+				case "3": tips(); break;
+				
+				case "4": virskārta(); break;
+				
+				case "5": summa(); break;
+				
+				case "x": JOptionPane.showMessageDialog(null, "Programma tika apturēta!"); break;
+				
+				default: JOptionPane.showMessageDialog(null, "Šāda darbība nepastāv!", "Kļūme!", JOptionPane.ERROR_MESSAGE); break;
+			}
+		}while(!izvele.equals("x"));
 	}
 }
