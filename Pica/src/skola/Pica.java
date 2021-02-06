fe63b7bd76917fe9ab28959da9da3c96fdeeaa5c
Endijs Dārznieks <endijsdarznieks@gmail.com>
2021-02-06 18:56:30 +0200

    Veikti vairāki uzlabojumi

--- Pica/src/skola/Pica.java
+++ Pica/src/skola/Pica.java
@@ -1,91 +1,92 @@
 package skola;
 import java.text.DecimalFormat;
-
 import javax.swing.JOptionPane;
 
 public class Pica {
 static boolean info = false;
 static String vards, uzvards, adrese, izvele, iznākumsT;
-static int numurs;
+static int numurs, atbilde;
 static Picērija Picērija = new Picērija();
-static double iznākumsL, galaSumma = 0;
+static double iznākumsL, galaSumma = 0, piegade = 0;
 static double[] iznākumsV;
+static String[] ja_ne = {"Jā", "Nē"};
 
 	static void informācija() {
-		vards = JOptionPane.showInputDialog("Ievadi savu vārdu: ");
-		uzvards = JOptionPane.showInputDialog("Ievadi savu uzvārdu: ");
-		numurs = Integer.parseInt(JOptionPane.showInputDialog("Ievadi savu tālruni: "));
-		adrese = JOptionPane.showInputDialog("Ievadi savu adresi: ");
-		info = true;
-		
-		String[] options = {"Jā", "Nē"};
-		int pareizi = JOptionPane.showOptionDialog(null, "Vārds/uzvārds: "+vards+" "+uzvards+"\n"+
-										                 "Tālrunis: "+numurs+"\nAdrese: "+adrese,
-										                 "Vai ievadītā informācija ir pareiza?", 0, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
-		if(pareizi == 1)
-			informācija();
+		do {
+			vards = JOptionPane.showInputDialog("Ievadi savu vārdu: ");
+			uzvards = JOptionPane.showInputDialog("Ievadi savu uzvārdu: ");
+			numurs = Integer.parseInt(JOptionPane.showInputDialog("Ievadi savu tālruni: "));
+			adrese = JOptionPane.showInputDialog("Ievadi savu adresi: ");
+			
+			info = true;
+			
+			atbilde = JOptionPane.showOptionDialog(null, "Vārds/uzvārds: "+vards+" "+uzvards+"\n"+
+											             "Tālrunis: "+numurs+"\nAdrese: "+adrese,
+											             "Vai ievadītā informācija ir pareiza?", 0, JOptionPane.QUESTION_MESSAGE, null, ja_ne, ja_ne[0]);
+		}while(atbilde == 1);
 	}
 	static void izveidotPicu() {
 		do {
-		do {
-			izvele = JOptionPane.showInputDialog("1 - Picas tips | 2 - Picas lielums | 3 - Papildus picas virskārtas/piedevas | 0 - Pabeigt picas taisīšanu");
+			do {
+				izvele = JOptionPane.showInputDialog("1 - Picas tips | 2 - Picas lielums | 3 - Papildus picas virskārtas/piedevas | 0 - Pabeigt picas taisīšanu");
+				
+				switch (izvele) {
+					case "1": iznākumsT = Picērija.tips(); break;
+					
+					case "2": iznākumsL = Picērija.lielums(); break;
+					
+					case "3": iznākumsV = Picērija.virskārtas(); break;
+					
+					case "0": if(iznākumsT != null && iznākumsL != 0) JOptionPane.showMessageDialog(null, "Pica tika izveidota!", "Ziņa", JOptionPane.INFORMATION_MESSAGE); break;
+					
+					default: JOptionPane.showMessageDialog(null, "Šāda darbība nepastāv!", "Kļūme!", JOptionPane.ERROR_MESSAGE); break;
+				}
+				
+			}while(!izvele.equals("0"));
 			
-			switch(izvele.toLowerCase()){
-				case "1": iznākumsT = Picērija.tips(); break;
-				
-				case "2": iznākumsL = Picērija.lielums(); break;
-				
-				case "3": iznākumsV = Picērija.virskārtas(); break;
-				
-				case "0": if(iznākumsT != null && iznākumsL != 0) JOptionPane.showMessageDialog(null, "Pica tika izveidota!", "Ziņa", JOptionPane.INFORMATION_MESSAGE); break;
-				
-				default: JOptionPane.showMessageDialog(null, "Šāda darbība nepastāv!", "Kļūme!", JOptionPane.ERROR_MESSAGE); break;
-			}
-			
-		}while(!izvele.equals("0"));
-		if(iznākumsT == null)
+			if(iznākumsT == null)
 				JOptionPane.showMessageDialog(null, "Nav izvēlēts picas tips!", "Ziņa", JOptionPane.INFORMATION_MESSAGE);
-		if(iznākumsL == 0)
-			JOptionPane.showMessageDialog(null, "Nav izvēlēts picas lielums!", "Ziņa", JOptionPane.INFORMATION_MESSAGE);
-		}while(iznākumsT == null || iznākumsL == 0);
+			if(iznākumsL == 0)
+				JOptionPane.showMessageDialog(null, "Nav izvēlēts picas lielums!", "Ziņa", JOptionPane.INFORMATION_MESSAGE);
+			
+			}while(iznākumsT == null || iznākumsL == 0);
 	}
 	static void summa() {
-		if(iznākumsT != null && iznākumsL != 0) {
-		double piegade = 0, iznākumsVKopā = 0;
-		String[] options = {"Jā", "Nē"};
-		DecimalFormat df = new DecimalFormat("#.##");
-		
-			int n = JOptionPane.showOptionDialog(null, "Vai vēlaties, lai piegādā uz norādīto adresi?\nPiegāde - 9.99€", "Piegāde", 0, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+		if(iznākumsT != null) {
+			piegade = 0;
+			double iznākumsVKopā = 0;
+			DecimalFormat df = new DecimalFormat("#.##");
 			
-			if(n == 0)
+			atbilde = JOptionPane.showOptionDialog(null, "Vai vēlaties, lai piegādā uz jūsu norādīto adresi?\nAdrese: "+adrese+"\nPiegāde - 9.99€", "Piegāde", 0, JOptionPane.QUESTION_MESSAGE, null, ja_ne, ja_ne[0]);
+				
+			if(atbilde == 0)
 				piegade = 9.99;
+				
+			if(iznākumsV != null) {
+				for(int i = 0; i < iznākumsV.length; i++)
+				iznākumsVKopā = iznākumsVKopā + iznākumsV[i];
 			
-		if(iznākumsV != null) {
-			for(int i = 0; i < iznākumsV.length; i++) {
-			iznākumsVKopā = iznākumsVKopā + iznākumsV[i];
-		}
-		galaSumma = piegade + iznākumsL + iznākumsVKopā;
-		}
-		else
-			galaSumma = piegade + iznākumsL;
-		
-		String[] options1 = {"Pasūtīt", "Atcelt"};
-		int izvele = JOptionPane.showOptionDialog(null, "Kopsumma euro = "+df.format(galaSumma)+"€", "Gala summa", 0, JOptionPane.INFORMATION_MESSAGE, null, options1, options1[0]);
-		
-		if(izvele == 0)
+			galaSumma = piegade + iznākumsL + iznākumsVKopā;
+			} else
+				galaSumma = piegade + iznākumsL;
+			
+			String[] opcijas = {"Pasūtīt", "Atcelt"};
+			int izvele = JOptionPane.showOptionDialog(null, "Izveidotā pica:\nTips: "+Picērija.iznākumsTips+"\nIzmērs: "+Picērija.iznākumsLielums+"\nPapildus piedevas: "+Picērija.piedevas+"\nPiegāde: "+piegade+"€"+
+															"\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\tKopsumma euro = "+df.format(galaSumma)+"€", "Gala summa", 0, JOptionPane.INFORMATION_MESSAGE, null, opcijas, opcijas[0]);
+			if(izvele == 0)
 				if(info == true) {
 					Picērija.rakstit();
-				}else
+				} else
 					JOptionPane.showMessageDialog(null, "Nav ievadīta kontaktinformācja!", "Ziņa", JOptionPane.INFORMATION_MESSAGE);
-		}else
+		} else
 			JOptionPane.showMessageDialog(null, "Nav izveidota pica!", "Ziņa", JOptionPane.INFORMATION_MESSAGE);
 	}
 	public static void main(String[] args) {
-		
 		do{
 			izvele = JOptionPane.showInputDialog("1 - Pasūtītāja informācija | 2 - Izveidot picu | 3 - Gala summa | 4 - Veiktie pasūtījumi | x - Apturēt programmu");
+			izvele = izvele.toLowerCase();
 			
-			switch(izvele.toLowerCase()){
+			switch (izvele) {
 				case "1": informācija(); break;
 				
 				case "2": izveidotPicu(); break;
@@ -96,7 +97,7 @@ static double[] iznākumsV;
 				
 				case "x": JOptionPane.showMessageDialog(null, "Programma tika apturēta!", "Ziņa", JOptionPane.INFORMATION_MESSAGE); break;
 				
-				default: JOptionPane.showMessageDialog(null, "Šāda darbība nepastāv!", "Kļūme!", JOptionPane.ERROR_MESSAGE); break;
+				default: JOptionPane.showMessageDialog(null, "Šāda darbība nepastāv!", "Kļūda!", JOptionPane.ERROR_MESSAGE); break;
 			}
 		}while(!izvele.equals("x"));
 	}
--- Pica/src/skola/Picērija.java
+++ Pica/src/skola/Picērija.java
@@ -6,13 +6,23 @@ import javax.swing.JOptionPane;
 public class Picērija {
 Object iznākumsObjektā;
 String iznākumsTips, iznākumsLielums, piedevas = "", failaAtrasanasVieta;
+int x = 1;
 
+	String tips() {
+		Object[] opcijas = {"Home Alone", "Hawaiian", "Pepperoni", "Hungry Man", "Meat Lovers", "Supreme"};
+		
+		iznākumsObjektā = JOptionPane.showInputDialog(null, "Veids:", "Izvēlies picas tipu/veidu", JOptionPane.PLAIN_MESSAGE, null, opcijas, opcijas[0]);
+        iznākumsTips = iznākumsObjektā.toString();
+        
+        return iznākumsTips;
+	}
+	
 	double lielums() {
 		double summaL = 0;
 		
-		Object[] options = {"Maza pica: 20-25 cm ar 6 šķēlēm - 11.65€", "Vidēja pica: 30 cm ar 8 šķēlēm - 14.15€",
+		Object[] opcijas = {"Maza pica: 20-25 cm ar 6 šķēlēm - 11.65€", "Vidēja pica: 30 cm ar 8 šķēlēm - 14.15€",
 							"Liela pica: 35 cm ar 10 šķēlēm - 18.31€", "Īpaši liela pica: 40-45 cm ar 12 šķēlēm - 19.98€"};
-        iznākumsObjektā = JOptionPane.showInputDialog(null, "Izmērs:", "Izvēlies picas izmēru", JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+        iznākumsObjektā = JOptionPane.showInputDialog(null, "Izmērs:", "Izvēlies picas izmēru", JOptionPane.PLAIN_MESSAGE, null, opcijas, opcijas[0]);
         iznākumsLielums = iznākumsObjektā.toString();
         
         if(iznākumsLielums.contains("11.65"))
@@ -26,44 +36,36 @@ String iznākumsTips, iznākumsLielums, piedevas = "", failaAtrasanasVieta;
         
         return summaL;
 	}
-	String tips() {
-		Object[] options = {"Home Alone", "Hawaiian", "Pepperoni", "Hungry Man", "Meat Lovers", "Supreme"};
-		
-		iznākumsObjektā = JOptionPane.showInputDialog(null, "Veids:", "Izvēlies picas tipu/veidu", JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
-        iznākumsTips = iznākumsObjektā.toString();
-        
-        return iznākumsTips;
-	}
+	
 	double[] virskārtas() {
 		int ja_ne, i = 0;
 		String cena = "";
 		piedevas = "";
 		
 		do {
-		ja_ne = 1;
-		
-		Object[] options = {"Nevienu", "Bekons - 4.19€", "Ananāsi - 2.19€", "Šķiņķis - 4.19€", "BBQ mērce - 3.29€", "Siers - 3.89€", "Sēnes - 2.19€", "Ķiploki - 0.36€", "Tomāti - 2.19€", "Sīpoli - 0.49€", "Melnie pipari - 0.33€", "Zaļie lociņi - 0.49€", "Karija mērce - 1.69€"};
-        iznākumsObjektā = JOptionPane.showInputDialog(null, "Piedevas:", "Izvēlies papildus picas virskārtas/piedevas", JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
-        String iznākumsVirskārtas = iznākumsObjektā.toString();
-        
-        if(iznākumsVirskārtas != "Nevienu") {
-        cena = cena + iznākumsVirskārtas.substring(iznākumsVirskārtas.length()-5, iznākumsVirskārtas.length()-1) + " ";
-        if(i > 0)
-        	piedevas = piedevas + "; ";
-        piedevas = piedevas + iznākumsVirskārtas;
-        }else {
-        	cena += "0 ";
-        }
-        
-        if(iznākumsVirskārtas != "Nevienu") {
-        	String[] options1 = {"Jā", "Nē"};
-        	ja_ne = JOptionPane.showOptionDialog(null, "Vai vēlaties pievienot vēl kādu piedevu?", "Jautājums", 0, JOptionPane.INFORMATION_MESSAGE, null, options1, options1[0]);
-        	i += 1;
-        }
-		}while(ja_ne == 0 );
+			ja_ne = 1;
+			
+			Object[] options = {"Nevienu", "Bekons - 4.19€", "Ananāsi - 2.19€", "Šķiņķis - 4.19€", "BBQ mērce - 3.29€", "Siers - 3.89€", "Sēnes - 2.19€", "Ķiploki - 0.36€", "Tomāti - 2.19€", "Sīpoli - 0.49€", "Melnie pipari - 0.33€", "Zaļie lociņi - 0.49€", "Karija mērce - 1.69€"};
+	        iznākumsObjektā = JOptionPane.showInputDialog(null, "Piedevas:", "Izvēlies papildus picas virskārtas/piedevas", JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+	        String iznākumsVirskārtas = iznākumsObjektā.toString();
+	        
+	        if(iznākumsVirskārtas != "Nevienu") {
+		        cena = cena + iznākumsVirskārtas.substring(iznākumsVirskārtas.length()-5, iznākumsVirskārtas.length()-1) + " ";
+		        
+		        if(i > 0)
+		        	piedevas = piedevas + "; ";
+		        
+		        piedevas = piedevas + iznākumsVirskārtas;
+		        
+		        ja_ne = JOptionPane.showOptionDialog(null, "Vai vēlaties pievienot vēl kādu piedevu?", "Jautājums", 0, JOptionPane.QUESTION_MESSAGE, null, Pica.ja_ne, Pica.ja_ne[0]);
+	        	i += 1;
+	        } else
+	        	cena += "0 ";
+	        
+		}while(ja_ne == 0);
 		
 		if(cena == "0 ")
-		i = 1;
+			i = 1;
 		
 		double[] summaV = new double[i];
 		String[] cenas = cena.split(" ");
@@ -73,8 +75,12 @@ String iznākumsTips, iznākumsLielums, piedevas = "", failaAtrasanasVieta;
 		
 		return summaV;
 	}
+	
 	void rakstit() {
-		failaAtrasanasVieta = JOptionPane.showInputDialog("Ieraksti adresi, kurā saglabāt failu ar veiktajiem pasūtījumiem: ") + "/Pasūtījumi.txt";
+		if(x == 1) {
+		failaAtrasanasVieta = JOptionPane.showInputDialog("Ieraksti adresi, kurā saglabāt failu ar veiktajiem pasūtījumiem: ") + "\\Pasūtījumi.txt";
+		x = JOptionPane.showOptionDialog(null, "Atcerēties šo adresi?\n"+failaAtrasanasVieta, "Adrese", 0, JOptionPane.QUESTION_MESSAGE, null, Pica.ja_ne, Pica.ja_ne[0]);
+		}
 		
 		try {
 			DecimalFormat df = new DecimalFormat("#.##");
@@ -84,32 +90,33 @@ String iznākumsTips, iznākumsLielums, piedevas = "", failaAtrasanasVieta;
 			if(piedevas == "")
 				piedevas = "Nav";
 			
-			raksta.println("\t\tPasūtījums - Pica:\nTips: "+iznākumsTips+"\nIzmērs: "+iznākumsLielums.substring(0, iznākumsLielums.length())+"\nPapildus piedevas: "+piedevas+
-					"\n\t\tGala summa = "+df.format(Pica.galaSumma)+"€\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+			raksta.println("\t\tPasūtījums - Pica:\nTips: "+iznākumsTips+"\nIzmērs: "+iznākumsLielums+"\nPapildus piedevas: "+piedevas+"\nPiegāde: "+Pica.piegade+"€"+
+						   "\n\t\tGala summa = "+df.format(Pica.galaSumma)+"€\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 			
 			raksta.close();
 		}catch (Exception e) {
 			JOptionPane.showMessageDialog(null, "Kļūda labojot failu!", "Kļūda!", JOptionPane.ERROR_MESSAGE);
 		}
 	}
+	
 	void nolasit() {
 		if(failaAtrasanasVieta != null) {
-		String teksts, pasūtījumi = "";
-		
-		try {
-			FileReader fr = new FileReader(failaAtrasanasVieta);
-			BufferedReader lasa = new BufferedReader(fr);
+			String teksts, pasūtījumi = "";
 			
-			while((teksts = lasa.readLine()) != null)
-				pasūtījumi += teksts + "\n";
-			
-			JOptionPane.showMessageDialog(null, pasūtījumi, "Veiktie pasūtījumi", JOptionPane.PLAIN_MESSAGE);
-			
-			lasa.close();
-		}catch (Exception e) {
-			JOptionPane.showMessageDialog(null, "Kļūda nolasot failu!", "Kļūda!", JOptionPane.ERROR_MESSAGE);
-		}
-		}else
+			try {
+				FileReader fr = new FileReader(failaAtrasanasVieta);
+				BufferedReader lasa = new BufferedReader(fr);
+				
+				while((teksts = lasa.readLine()) != null)
+					pasūtījumi += teksts + "\n";
+				
+				JOptionPane.showMessageDialog(null, pasūtījumi, "Veiktie pasūtījumi", JOptionPane.PLAIN_MESSAGE);
+				
+				lasa.close();
+			}catch (Exception e) {
+				JOptionPane.showMessageDialog(null, "Kļūda nolasot failu!", "Kļūda!", JOptionPane.ERROR_MESSAGE);
+			}
+		} else
 			JOptionPane.showMessageDialog(null, "Nav veikti pasūtījumi!", "Ziņa", JOptionPane.INFORMATION_MESSAGE);
 	}
 }
